7d0f0b15f976fd5f9a2446bcfc85248fad7aa0af
Webtroter <Webtroter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-14 05:26:06 +0000

    Fix bug compatibility wordpress 3.6
    git-svn-id: https://plugins.svn.wordpress.org/automatic-nbsp/tags/1.1@755922 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin.php
@@ -0,0 +1,61 @@
+<?php
+add_action('admin_menu', 'dg_automatic_nbsp_menu');
+
+function dg_automatic_nbsp_menu() {
+    add_options_page('Automatic NBSP options', 'Automatic NBSP', 'manage_options', 'dg_automatic_nbsp_id', 'dg_automatic_nbsp_options');
+}
+
+function dg_automatic_nbsp_options() {
+
+    if (!current_user_can('manage_options')) {
+        wp_die(__('You do not have sufficient permissions to access this page.'));
+    }
+
+    $conjunctions = 'dg_automatic_nbsp';
+    $hidden_field_name = 'dg_submit_hidden';
+    $data_field_name = 'dg_conjunction';
+
+    // Read in existing option value from database
+    $opt_val = get_option($conjunctions);
+
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if (isset($_POST[$hidden_field_name]) && $_POST[$hidden_field_name] == 'Y') {
+        // Read their posted value
+        $opt_val = $_POST[$data_field_name];
+
+        // Save the posted value in the database
+        update_option($conjunctions, $opt_val);
+        ?>
+
+
+        <?php
+    }
+
+    // Now display the settings editing screen
+
+    echo '<div class="wrap">';
+
+    // header
+
+    echo "<h2>" . __('Automatic NBSP settings') . "</h2>";
+
+    // settings form
+    ?>
+
+    <form name="form1" method="post" action="">
+        <input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
+
+        <p><?php _e("List of words that you want to move to the next line. Separate by comma."); ?> 
+            <input type="text" name="<?php echo $data_field_name; ?>" value="<?php echo $opt_val; ?>" size="80">
+        </p><hr />
+
+        <p class="submit">
+            <input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+        </p>
+
+    </form>
+    </div>
+
+    <?php
+} ?>
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Automatic NBSP ===
+Contributors: Damian Góra
+Tags: typography, posts, nbsp
+Requires at least: 3.0
+Tested up to: 3.6
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Automatic NBSP adds HTML non-breaking space (&nbsp) in your posts, custom post types and pages.
+
+== Description ==
+
+This plugin automatically adds HTML non-breaking space (&nbsp) in your posts, custom post types and pages. 
+In settings of plugin you can add words. Each added word will not be displayed on the end of line in text, but was moved to beginning of next line. 
+Automatic NBSP allows You to force the browser to not break the line between certain words or web page elements. E.g. 'Mr. Someone' should be 'Mr.&nbsp;Someone'. 
+Non-Breaking Space is particularly important in Polish and Czech languages. In these countries there is a typographic rule, which prohibits the display some conjunctions and prepositions on the end line of text. In other cases, help You control the aesthetics.
+
+== Installation ==
+
+1. Install the plugin from within the Dashboard or upload the directory `widget-or-sidebar-per-shortcode` and all its contents to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Changelog ==
+
+= 1.1 =
+Fix bug compatibility wordpress 3.6
+
+= 1.0 =
+First version.
