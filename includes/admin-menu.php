fa48159442dc0bee56d90a0d2fc431ebfc353118
Webtroter <Webtroter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-14 17:19:41 +0000

    tagging version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/automatic-nbsp/tags/1.2@1134669 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/admin-menu.php
@@ -0,0 +1,10 @@
+<?php
+add_action('admin_menu', 'dgwt_nbsp_admin_menu');
+
+function dgwt_nbsp_admin_menu() {
+
+    add_submenu_page('options-general.php', __('Automatic NBSP options', 'automatic-nbsp'), __('Automatic NBSP', 'automatic-nbsp'), 'manage_options', 'dgwt_nbsp_options', 'dgwt_nbsp_options_page');
+    
+}
+
+?>
